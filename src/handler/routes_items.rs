bcdc93a89314bed581b6e8bcd2cc257c893d7f05
ligulfzhou <ligulfzhou53@gmail.com>
2023-11-05 15:54:31 +0800

    routes_items

--- src/handler/routes_items.rs
+++ src/handler/routes_items.rs
@@ -1,8 +1,9 @@
 use crate::dto::dto_items::{DeleteParams, EditParams, QueryParams};
 use crate::model::items::ItemsModel;
-use crate::response::api_response::APIListResponse;
+use crate::response::api_response::{APIEmptyResponse, APIListResponse};
+use crate::service::item_service::ItemServiceTrait;
 use crate::state::item_state::ItemState;
-use crate::ERPError;
+use crate::{ERPError, ERPResult};
 use axum::extract::{Query, State};
 use axum::routing::{get, post};
 use axum::Router;
@@ -17,21 +18,25 @@ pub fn routes() -> Router<ItemState> {
 
 async fn api_item_list(
     State(state): State<ItemState>,
-    WithRejection(Query(param), _): WithRejection<Query<QueryParams>, ERPError>,
-) -> Result<APIListResponse<ItemsModel>, ERPError> {
-    todo!()
+    WithRejection(Query(params), _): WithRejection<Query<QueryParams>, ERPError>,
+) -> ERPResult<APIListResponse<ItemsModel>> {
+    let items = state.item_service.get_item_list(&params).await?;
+    let count = state.item_service.get_item_count(&params).await?;
+    Ok(APIListResponse::new(items, count))
 }
 
 async fn api_item_edit(
     State(state): State<ItemState>,
-    WithRejection(Query(param), _): WithRejection<Query<EditParams>, ERPError>,
-) -> Result<APIListResponse<ItemsModel>, ERPError> {
-    todo!()
+    WithRejection(Query(params), _): WithRejection<Query<EditParams>, ERPError>,
+) -> ERPResult<APIEmptyResponse> {
+    state.item_service.edit_item(&params).await?;
+    Ok(APIEmptyResponse::new())
 }
 
 async fn api_item_delete(
     State(state): State<ItemState>,
-    WithRejection(Query(param), _): WithRejection<Query<DeleteParams>, ERPError>,
-) -> Result<APIListResponse<ItemsModel>, ERPError> {
-    todo!()
+    WithRejection(Query(params), _): WithRejection<Query<DeleteParams>, ERPError>,
+) -> ERPResult<APIEmptyResponse> {
+    state.item_service.delete_item(&params).await?;
+    Ok(APIEmptyResponse::new())
 }

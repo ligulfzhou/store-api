5b79882598dfd19a4cd3ca2359fd48f6f1c6a17b
ligulfzhou <ligulfzhou53@gmail.com>
2023-11-11 16:35:52 +0800

    customer_param remove customer_no and qq fields

--- src/dto/dto_customer.rs
+++ src/dto/dto_customer.rs
@@ -114,14 +114,12 @@ impl CustomerSearchParam {
 #[derive(Debug, Deserialize)]
 pub struct CustomerEditParam {
     pub id: Option<i32>,
-    pub customer_no: String,
     pub ty_pe: i32,
     pub name: String,
     pub head: String,
     pub address: String,
     pub email: String,
     pub birthday: Option<NaiveDate>,
-    pub qq: String,
     pub phone: String,
     pub notes: String,
 }

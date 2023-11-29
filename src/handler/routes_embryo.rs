948c0957c424dee2eca5f6c949d359259f510863
ligulfzhou <ligulfzhou53@gmail.com>
2023-11-29 16:52:46 +0800

    Extension参与需要放在第二位

--- src/handler/routes_embryo.rs
+++ src/handler/routes_embryo.rs
@@ -16,13 +16,15 @@ pub fn routes() -> Router<EmbryoState> {
         .route("/api/embryos", get(api_item_list))
         .route("/api/embryo/edit", post(api_item_edit))
         .route("/api/embryo/delete", post(api_item_delete))
-    // .route("/api/embryo/inout", post(api_item_inout))
+        .route("/api/embryo/inout", post(api_item_inout))
 }
 
 async fn api_item_list(
     State(state): State<EmbryoState>,
     WithRejection(Query(params), _): WithRejection<Query<QueryParams>, ERPError>,
 ) -> ERPResult<APIListResponse<EmbryoModel>> {
+    tracing::info!("api_item_list : /api/embryos");
+
     let items = state.embryo_service.get_item_list(&params).await?;
     let count = state.embryo_service.get_item_count(&params).await?;
     Ok(APIListResponse::new(items, count))
@@ -32,6 +34,8 @@ async fn api_item_edit(
     State(state): State<EmbryoState>,
     WithRejection(Json(params), _): WithRejection<Json<EditParams>, ERPError>,
 ) -> ERPResult<APIEmptyResponse> {
+    tracing::info!("api_item_edit : /api/embryo/edit");
+
     state.embryo_service.edit_item(&params).await?;
     Ok(APIEmptyResponse::new())
 }
@@ -40,15 +44,18 @@ async fn api_item_delete(
     State(state): State<EmbryoState>,
     WithRejection(Json(params), _): WithRejection<Json<GenericDeleteParams>, ERPError>,
 ) -> ERPResult<APIEmptyResponse> {
+    tracing::info!("api_item_delete : /api/embryo/delete");
+
     state.embryo_service.delete_item(&params).await?;
     Ok(APIEmptyResponse::new())
 }
 
 async fn api_item_inout(
     State(state): State<EmbryoState>,
-    WithRejection(Json(params), _): WithRejection<Json<InoutParams>, ERPError>,
     Extension(account): Extension<AccountDto>,
+    WithRejection(Json(params), _): WithRejection<Json<InoutParams>, ERPError>,
 ) -> ERPResult<APIEmptyResponse> {
+    tracing::info!("api_item_inout : /api/embryo/inout");
     state
         .embryo_service
         .add_item_inout(&params, account.id)
